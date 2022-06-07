bd0b70a233f36edc7f264423f5fcdf0790621e91
trungtashi99 <trungtashi@gmai.com>
2022-06-07 16:23:16 +0700

    Update chuyển đổi tiền tệ

--- script.js
+++ script.js
@@ -2,22 +2,11 @@ function convert(){
     let amount = document.getElementById('amount').value;
     let from = document.getElementById('from').value;
     let to = document.getElementById('to').value;
-    let result
-
-    if(from == "vnd" && to == "usd"){
-        result = 'Result: ' + (amount/23000) + ' USD'
-    }
-    else if (from == "usd" && to == "vnd") {
-        result = 'Result: ' + (amount*23000) + ' VND'
-    }
-    else if(from == 'vnd'){
-        result = 'Result: '+amount+' VND'
-    }
-    else{
-        result = 'Result: '+amount+' USD' 
-    }
+    let rate = from/to
+    let result = amount/rate
 
     document.getElementById('result').innerHTML = result
+
 }
 
 
